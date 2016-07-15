eaf230276a3b146a6b79e3b11e10ec2db54d5289
Mahesh Gahlot <gahlotmahesh8@gmail.com>
2016-07-15 09:49:38 +0530

    Adding required Java files

---
+++ MyBot.java
@@ -0,0 +1,19 @@
+import org.jibble.pircbot.*;
+
+public class MyBot extends PircBot {
+
+    public MyBot(){
+                this.setName("Smarty");
+
+    }
+
+        public void onMessage(String channel, String sender,String login, String hostname, String message) {
+            if (message.equalsIgnoreCase("time")) {
+                            String time = new java.util.Date().toString();
+                            sendMessage(channel, sender + ": The time is now " + time);
+
+            }
+
+        }
+
+}
---
+++ MyBotMain.java
@@ -0,0 +1,21 @@
+import org.jibble.pircbot.*;
+
+public class MyBotMain {
+    
+    public static void main(String[] args) throws Exception {
+        
+        // Now start our bot up.
+        MyBot bot = new MyBot();
+        
+        // Enable debugging output.
+        bot.setVerbose(true);
+        
+        // Connect to the IRC server.
+        bot.connect("irc.freenode.net");
+
+        // Join the #pircbot channel.
+        bot.joinChannel("#pircbot");
+        
+    }
+    
+}
